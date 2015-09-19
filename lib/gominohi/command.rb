fce42839f88285bc236a4209103819a29cd2f3cc
irasally <irasally@gmail.com>
2015-09-19 15:58:20 +0900

    special_orderはsourceファイルに定義しておきたい

--- lib/gominohi/command.rb
+++ lib/gominohi/command.rb
@@ -14,20 +14,8 @@ module Gominohi
     end
 
     desc "generate PLACE BEGIN END [1-4]", "Generate garbage days."
-    def generate(place, begin_date, end_date, order=1)
-      case order.to_i
-      when 1
-        special_order = [:paper, :not_burn, :paper, :leaf]
-      when 2
-        special_order = [:leaf, :paper, :not_burn, :paper]
-      when 3
-        special_order = [:paper, :leaf, :paper, :not_burn]
-      when 4
-        special_order = [:not_burn, :paper, :leaf, :paper]
-      else
-        raise ArgumentError, "特殊曜日の順序が不正です。"
-      end
-      puts Generator.__send__(place, begin_date, end_date, special_order)
+    def generate(place, begin_date, end_date)
+      puts Generator.__send__(place, begin_date, end_date)
     end
   end
 end
